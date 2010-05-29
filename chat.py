71afad3f9a4fb354257a9333d6631276de2f43d0
Bruno Croci <crocidb@gmail.com>
2010-05-29 19:20:37 -0300

    Some changes...

--- chat.py
+++ chat.py
@@ -4,7 +4,11 @@ from chatwindow import ChatWindow
 
 class Chat:
 	def __init__(self):
+		# Main Window
 		self.window = ChatWindow(self)
+
+		# Socket Controll
+		self.sock_control = SocketControl()
 		
 	def show(self):
 		self.window.show()
